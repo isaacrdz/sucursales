3bcc1e1022970a24df0858e56d67cf3672c86467
isaacrdz <zack_cpp@hotmail.com>
2014-06-02 17:09:55 -0500

    update

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
       };
 
       (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-       FB.Canvas.setSize({ width: 810, height: 1200 });  
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];  
+   if (d.getElementById(id)) {return;}  
+        js = d.createElement('script'); js.id = id; js.async = true;  
+           js.src = "//connect.facebook.net/en_US/all.js";  
+           ref.parentNode.insertBefore(js, ref);  
+ }(document));  
+ FB.Canvas.setSize({ width: 810, height: 1200 });  
     </script>
 
 </head>
