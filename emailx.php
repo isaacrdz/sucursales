4585a52f4654624b90c74e2d4994b7706a6e96bf
isaacrdz <zack_cpp@hotmail.com>
2015-01-21 11:05:09 -0600

    remove

--- emailx.php
+++ emailx.php
@@ -9,52 +9,16 @@
 
 
 <section id="form-box">
-       <form action="emaixl.php" method="post">
-          <fieldset>
-            <h2>¿Necesitas Información?</h2>
-            <label> Nombre: </label>
-            <input placeholder="Obligatorio" type="text" name="nombre" id="nombre" title="Nombre" maxlength="60" autocomplete="off" required />
-            <br/>
-            <label>   Correo: </label>
-            <input type="email" name="email" id="email" title="correo" required  align="right">
-            </input>
-            <br />
-            <label> Teléfono*: </label>
-            <input placeholder="Obligatorio" value="Obligatorio" type="tel" name="tel" id="tel" required>
-            </input>
-            <br>
-            <br>
-            <label>Cómo se enteró de nosotros?</label>
-            <select name="como">
-              <option value="Buscador">Google</option>
-              <option value="Facebook">Facebook</option>
-              <option value="Twitter">Twitter</option>
-              <option value="TV">Youtube</option>
-              <option value="TV">TV</option>
-              <option value="Amigo">Amigo</option>
-              <option value="Prensa">Prensa</option>
-              <option value="Seccion">Sección Amarilla</option>
-              <option value="Radio">Radio</option>
-              <option value="Volante">Volante</option>
-              <option value="Cliente">Cliente Anterior</option>
-            </select> 
-            <br>           
-            <label> Comentario: </label>
-            <br />
-            <textarea placeholder="Obligatorio" cols="28" rows="10" name="mensaje"></textarea>
-            <br />
-            <input type="submit" value="ENVIAR" name="submit1" id="submit" />  
-            <p>Mensaje enviado correctamente</p>          
-          </fieldset>
+   Gracias
             
    </section>
 
    <?
 // configuración
-$mensajeenviado='Enviado correctamente, en breve nos comunicaremos con usted';
-$redireccion='http://maquinariarivero.com'; // cuando se envie el mail hacia donde me tengo que ir, eso dice el archivo jeje
-$mensajenoenviado='Favor de completar las palabras correctamente';
-$redireccionx='http://maquinariarivero.com/contacto-maquinaria-construccion-liugong-genie-monterrey-mexico.php'; // cuando se envie el mail hacia donde me tengo que ir, eso dice el archivo jeje
+//$mensajeenviado='Enviado correctamente, en breve nos comunicaremos con usted';
+//$redireccion='http://maquinariarivero.com'; // cuando se envie el mail hacia donde me tengo que ir, eso dice el archivo jeje
+//$mensajenoenviado='Favor de completar las palabras correctamente';
+//$redireccionx='http://maquinariarivero.com/contacto-maquinaria-construccion-liugong-genie-monterrey-mexico.php'; // cuando se envie el mail hacia donde me tengo que ir, eso dice el archivo jeje
 
 // enviamos el email
 
